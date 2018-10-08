a9e018fea2a3fb502c2ba44143a39dbcd402a5ee
vaidas100 <vaidas100@gmail.com>
2018-10-08 14:46:19 +0300

    Change test file path

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import sys
 import unittest
 from unittest import TestCase
@@ -15,6 +16,8 @@ logger = logging.getLogger()
 if sys.version_info[0] < 3:
     logger.error("Must be using Python 3")
     exit(1)
+
+root_dir = os.path.abspath(os.path.dirname(__file__))
 
 
 class TestAmericanSoundex(TestCase):
@@ -47,7 +50,7 @@ class TestAmericanSoundex(TestCase):
 class TestFileChunkReader(TestCase):
 
     def test_get_chunk(self):
-        file_path = "file_chunk_reader/__init__.py"
+        file_path = os.path.join(root_dir, "file_chunk_reader/__init__.py")
         fcr = FileChunkReader(file_path, chunk_size=10)
         file_chunk_text0 = fcr.get_chunk(0)
         file_chunk_text1 = fcr.get_chunk(1)
@@ -56,7 +59,7 @@ class TestFileChunkReader(TestCase):
         self.assertEqual(file_chunk_text1, b"_chunk_rea")
 
     def test_get_chunk_with_shift_depending_on_spaces(self):
-        file_path = "file_chunk_reader/__init__.py"
+        file_path = os.path.join(root_dir, "file_chunk_reader/__init__.py")
         fcr = FileChunkReader(file_path, chunk_size=20)
         file_chunk_text0 = fcr.get_chunk_with_shift_depending_on_spaces(0)
         file_chunk_text1 = fcr.get_chunk_with_shift_depending_on_spaces(1)
